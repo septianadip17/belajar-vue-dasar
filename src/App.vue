19f133c5936b176a7f28711dae2e87bfb6a46ebe
Septian Adi Pratama <septianadip7998@gmail.com>
2025-09-01 19:50:03 +0700

    Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+const isLoggedIn = true
+const users = ["Budi", "Siti", "Andi"]
+const message = "Hello Vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <!-- v-if -->
+  <p v-if="isLoggedIn">Selamat datang!</p>
+  <p v-else>Silakan login</p>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <!-- v-for -->
+  <ul>
+    <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+  </ul>
+
+  <!-- v-model -->
+  <input v-model="message" />
+  <p>Pesan: {{ message }}</p>
+  <HelloWorld name="Budi" />
+  <HelloWorld name="Siti" />
+  <HelloWorld name="Andi" />
+</template>
