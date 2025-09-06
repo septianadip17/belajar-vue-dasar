e75033459d68565493a419509ac47ed6b1f05c63
Septian Adi Pratama <septianadip7998@gmail.com>
2025-09-06 23:34:45 +0700

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { ref } from "vue"
 
-const isVisible = ref(true)
+const categories = ref([
+  { name: "Buah", items: ["Apel", "Mangga", "Jeruk"] },
+  { name: "Sayur", items: ["Bayam", "Wortel", "Tomat"] }
+])
 </script>
 
 <template>
-  <div>
-    <p v-show="isVisible">Teks ini bisa disembunyikan dengan v-show</p>
-    <button @click="isVisible = !isVisible">Toggle</button>
+  <div v-for="category in categories" :key="category.name">
+    <h4>{{ category.name }}</h4>
+    <ul>
+      <li v-for="item in category.items" :key="item">{{ item }}</li>
+    </ul>
   </div>
 </template>
