e21f488975cd67cb5a0eae2d23fc6cf3ffe381f8
Septian Adi Pratama <septianadip7998@gmail.com>
2025-09-06 17:07:55 +0700

    use ref

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 const isLoggedIn = true
 const users = ["Budi", "Siti", "Andi"]
 const message = "Hello Vue"
+import { ref } from "vue"
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
 </script>
 
 <template>
@@ -19,4 +26,12 @@ const message = "Hello Vue"
   <input v-model="message" />
   <p>Pesan: {{ message }}</p>
   <HelloWorld name="Budi" age="30" isAdmin />
+
+  <!-- ref -->
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">Tambah</button>
+  </div>
+
+  
 </template>
