3ae4adc7a0149511eec28845f71c9f6e17826ee2
Septian Adi Pratama <septianadip7998@gmail.com>
2025-09-09 23:21:54 +0700

    nested loop

---
+++ src/study/NestedLoop.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue"
+
+const categories = ref([
+  { name: "Buah", items: ["Apel", "Mangga", "Jeruk"] },
+  { name: "Sayur", items: ["Bayam", "Wortel", "Tomat"] }
+])
+</script>
+
+<template>
+  <div v-for="category in categories" :key="category.name">
+    <h4>{{ category.name }}</h4>
+    <ul>
+      <li v-for="item in category.items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
